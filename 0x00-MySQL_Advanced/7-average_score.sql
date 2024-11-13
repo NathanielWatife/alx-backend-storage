91bc0844ad93ee77d131b538f4b6a9c463093b64
NathanielWatife <awosanminathaniel0@gmail.com>
2024-11-13 23:40:20 +0100

    stored a procedure ComputerAverage

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,21 @@
+-- Create stored procedure ComputeAverageScoreForUser
+DELIMITER $$
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN user_id INT
+)
+BEGIN
+    DECLARE avg_score FLOAT;
+
+    SELECT AVG(score) INTO avg_score
+    FROM corrections
+    WHERE corrections.user_id = user_id;
+
+    UPDATE users
+    SET average_score = avg_score
+    WHERE users.id = user_id;
+END$$
+DELIMITER ;
